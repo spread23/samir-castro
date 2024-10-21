44dce43da8cfeb9ec9a933c82b300ad2f3f4fcfb
Samir <spreadgrafica@gmail.com>
2024-10-21 10:59:18 -0500

    adding social media icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-import samir from './assets/samir_castro.jpg'
+import samir from './assets/samir_castro_logo.png'
+import { FaInstagram } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa"
+import { FaTiktok } from "react-icons/fa";
 
 import './app.css'
 
@@ -12,10 +15,19 @@ export const App = () => {
           <img className='image' src={samir} alt="samir" />
         </div>
         <div className='description-container'>
+          <h1 className='title-logo'>Samir Castro</h1>
           <h2 className='title-description'>Servicios:</h2>
           <a href='https://bugfixerpro.com/' target='_blank' className='btn-description'>Bug Fixer Pro</a>
-          <a className='btn-description-two'>Más servicios</a>
+          <a className='btn-description-two'>Blog</a>
           <a className='btn-description-three'>Más servicios</a>
+        </div>
+
+        <div className='footer-container'>
+          <div className='social-media'>
+            <a href="#"><FaInstagram className='icon'></FaInstagram></a>
+            <a href="#"><FaFacebook className='icon'></FaFacebook></a>
+            <a href="#"><FaTiktok className='icon'></FaTiktok></a>
+          </div>
         </div>
       </div>
     </>
